2393f7c50ec3374b51b9ab094e442c6adcc63502
stebrc <stefanuzzo22@gmail.com>
2025-06-11 13:25:58 +0200

    Remove odom_to_dvl node implementation and related header file from the project

--- src/odom_to_dvl.cpp
+++ src/odom_to_dvl.cpp
@@ -1,5 +1,25 @@
-#include "odom_to_dvl.hpp"
+#include <rclcpp/rclcpp.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+#include <geometry_msgs/msg/twist_with_covariance_stamped.hpp>
+#include <random>
 #include <functional>
+
+class OdomToDvlNode : public rclcpp::Node
+{
+public:
+  OdomToDvlNode();
+
+private:
+  void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg);
+
+  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr subscription_;
+  rclcpp::Publisher<geometry_msgs::msg::TwistWithCovarianceStamped>::SharedPtr publisher_;
+  
+  double noise_std_dev_;
+  
+  std::default_random_engine random_generator_;
+  std::normal_distribution<double> normal_distribution_;
+};
 
 using std::placeholders::_1;
 
