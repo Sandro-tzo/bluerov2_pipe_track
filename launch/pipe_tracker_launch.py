1a56b96f401de3afafacb2d0007fab992a6f2c2a
stebrc <stefanuzzo22@gmail.com>
2025-06-13 13:20:18 +0200

    Remove odom_to_tf node and update odometry topic remapping in launch file

--- launch/pipe_tracker_launch.py
+++ launch/pipe_tracker_launch.py
@@ -10,31 +10,18 @@ def generate_launch_description():
 
     with sl.group(ns='bluerov2'):
 
-        # Nodo Odom->TF: ora sa che il frame genitore è 'world'
-        sl.node(
-            package='bluerov2_pipe_track',  # <<< USA IL NOME DEL TUO PACCHETTO
-            executable='odom_to_tf',
-            name='odom_to_tf_publisher',
-            parameters=[
-                {'odom_topic': 'odom'},
-                # Forziamo il frame corretto letto dal simulatore
-                {'fallback_parent_frame': 'world'} 
-            ]
-        )
-        
         # Nodo Tracker Autonomo: ora sa che deve pubblicare setpoint nel frame 'world'
         sl.node(
-            package='bluerov2_pipe_track',  # <<< USA IL NOME DEL TUO PACCHETTO
+            package='bluerov2_pipe_track',
             executable='pipe_tracker',
             name='autonomous_tracker',
             output='screen',
             parameters=[
                 {'image_topic': 'image'},
-                {'odom_topic': 'odom'},
+                {'odom_topic': '/bluerov2/ukf/odom'},
                 {'velocity_setpoint_topic': 'cmd_vel'},
                 {'pose_setpoint_topic': 'cmd_pose'},
                 {'robot_frame_id': 'bluerov2/base_link'},
-                # CORREZIONE FONDAMENTALE QUI
                 {'world_frame_id': 'world'}, 
                 {'depth_setpoint': -8.0},
                 # ... etc ...
@@ -48,7 +35,7 @@ def generate_launch_description():
             name='hinf_controller',
             output='screen',
             remappings=[
-                ('~/odom', 'odom'),
+                ('odom', '/bluerov2/ukf/odom'),
                 ('~/pose_setpoint', '/bluerov2/cmd_pose'),
                 ('~/velocity_setpoint', '/bluerov2/cmd_vel'),
                 ('~/wrench_command', 'thruster_manager/input_wrench')
